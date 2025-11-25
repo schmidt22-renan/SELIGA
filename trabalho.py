7d4334d9a49df302ea6bf833155ddad1d9cc86d1
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-11-25 12:00:33 +0000

    f

---
+++ trabalho.py
@@ -0,0 +1,6 @@
+# fazendo calculo da idadxe do usuario com o ano de nacimento
+
+ano_nacimento = int(input("Digite seu ano de nacimento: "))
+idade = 2025 - ano_nacimento
+
+print("sua idade e de" ,idade ,anos)
