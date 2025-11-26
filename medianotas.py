da6cb95c9a99f05265ee24da039b3fab20599325
schmidt22-renan <schmidt.renan@escola.pr.gov.br>
2025-11-26 14:41:36 +0000

    feito.

---
+++ medianotas.py
@@ -0,0 +1,9 @@
+nota1 = float(input('Digite a primeira nota'))
+nota2= float(input('Digite a segunda nota'))
+media = (nota1 + nota2) / 2
+if media >=6:
+         print('aluno aprovado!')
+elif media >= 4:
+                print('Aluno emrecuperaçao')
+else:
+                print('Aluno reprovado. ')
